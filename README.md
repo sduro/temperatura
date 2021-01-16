2564fd9a187a35fdd82a8ad01d299542f07b5ce3
sduro <sergiduro@gmail.com>
2021-01-16 20:04:54 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# temperatura
-Temperatura/Humedad DHT11
+# Temperatura
+Raspberry con el sensor de temperatura y humedad DHT11 (+/-1ºC)
+## Temperatura interna
+La temperatura de la CPU la obtiene de '/opt/vc/bin/vcgencmd measure_temp'
+
+## Temperatura externa
+Hace la lectura del sensor DHT11.
+
+## Humedad
+Lectura de humedad del senser DHT11.
--- datagen.py
+++ datagen.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+#Aplicacion para adquirir la temperatura de la raspberry, y del sen
+#sensor DHT11 (no dispone de decimales)
+
 import sys, os, logging, urllib, datetime
 import Adafruit_DHT
 
-sensor = Adafruit_DHT.DHT11
+#opciones Adafruit_DHT.DHT11/Adafruit_DHT.DHT22 
+sensor = Adafruit_DHT.DHT11 #modelo actual instalado en la raspberry.
 pin = 17 #pin 11 ->GPIO17
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
@@ -18,9 +22,10 @@ format = "%Y-%m-%d,%H:%M:%S"
 today = datetime.datetime.today()
 s = today.strftime(format)
 
+#datos que quedaran registrados en el fichero tempdata.dat
 output = s+' '+fetchtemp()+' '+str(temperature)+' '+str(humidity)+'\n'
 
 print(output)
-
+#escritura en el fichero.
 with open('/home/pi/Documents/plottemp/tempdata.dat', 'a') as f:
 	f.write(output)
